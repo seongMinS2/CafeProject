ffd8f8ea9b40a1e5995c5c30d915a395f83a77ca
alikwon <63032589+alikwon@users.noreply.github.com>
2020-06-24 22:01:21 +0900

    Update kjj.js

---
+++ js/kjj.js
@@ -0,0 +1,30 @@
+// const a = $("div.swiper-container.center-block1.swiper2>.swiper-wrapper > .swiper-slide");
+// var b = document.querySelector(".swiper-container.center-block1 .swiper-wrapper");
+// // var c = b.querySelector("div.swiper-wrapper");
+// function init(){
+//     console.log("e");
+//     var addHTML =''; 
+//     addHTML+='<div class="swiper-slide">';
+//     addHTML+='  <div class="container">';
+//     addHTML+='          <div class="col-sm-5 ml-3  mt-3  menu1">';
+//     addHTML+='              <img class="hvr-grow" src="../images/image.JPG" class="rounded">';
+//     addHTML+='              <div class="text-center mt-3">아메리카노</div>';
+//     addHTML+='              <div class="text-center">&#8361;5,000</div></div>';
+//     addHTML+='          <div class="col-sm-5 ml-5 mt-3 menu1">';
+//     addHTML+='              <img src="../images/image.JPG" class="rounded">';
+//     addHTML+='              <div class="text-center mt-3">아메리카노</div>';
+//     addHTML+='              <div class="text-center">&#8361;5,000</div></div>';
+//     addHTML+='          <div class="col-sm-5 ml-3 mt-5 menu1">';
+//     addHTML+='              <img src="../images/image.JPG" class="rounded">';
+//     addHTML+='              <div class="text-center mt-3">아메리카노</div>';
+//     addHTML+='              <div class="text-center">&#8361;5,000</div>';
+//     addHTML+='          </div>';
+//     addHTML+='      </div>';
+//     addHTML+='   </div>';
+//     addHTML+='</div>';
+
+//     a.after("eeee")
+//     a.after(addHTML);
+
+// }
+// init();
