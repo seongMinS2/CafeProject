cffe701b4311fdb682e73681ec9cadfe0db5c3b7
marine <hollyzero@naver.com>
2020-06-24 17:15:17 +0900

    최신버전

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
 var menus = [];
 
+
+// 메뉴 클릭 시 색깔 이벤트
+$(document).ready(function () {
+    $("li").each(function () {
+        $(this).click(function () {
+            $(this).addClass("selected");               //클릭된 부분을 상단에 정의된 CCS인 selected클래스로 적용
+            $(this).siblings().removeClass("selected");  //siblings:형제요소들,    removeClass:선택된 클래스의 특성을 없앰
+        });
+    });
+});
+
+// 메인 메뉴 클릭 시 리셋
+$('.navbar-brand').click(function (e){
+        location.reload();
+});
+
 //커피 클릭 이벤트
 $('#1').click(function (e) {
     $('#1').parent().addClass('active');
@@ -68,3 +84,4 @@ function init() {
 };
 
 init();
+
