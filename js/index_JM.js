e300fc571f74fc6cbf36b259572f1711e539a2dc
PCBiS <63032526+PCBiS@users.noreply.github.com>
2020-06-24 15:28:31 +0900

    마스터 푸시 전 파일
    11123

--- js/index_JM.js
+++ js/index_JM.js
@@ -8,30 +8,41 @@ var menus = [];
 $('div.col-sm-2.menu1.ml-5.mt-5').click(function (e) {
    var target = $('p',this).text();
    var addHTML = '';
-   var tempCnt = '', tempPrice = '';
-   for (var i = 0; i < menus.length; i++) {
-        if (target == menus[i].name && document.getElementById(menus[i].name) == null) {
-            addHTML += '<div class = "row" id="' + menus[i].name + '">';
-                addHTML += '<div class = "col-sm-5 name-tag productName">';
-                    addHTML += '<p>' + menus[i].name + '</p>';
-                addHTML += '</div>';                    
-                addHTML += '<div class = "col-sm-4 name-tag productCount">';
-                    addHTML += '<p>' + menus[i].cnt + '</p>';
+
+   if(document.getElementsByClassName('.productName').innerText != target){
+        for (var i = 0; i < menus.length; i++) {
+            if (target == menus[i].name && document.getElementById(menus[i].id) == null) {
+                addHTML += '<div class = "row" id="' + menus[i].id + '">';
+                    addHTML += '<div class = "col-sm-5 name-tag productName">';
+                        addHTML += '<p>' + menus[i].name + '</p>';
+                    addHTML += '</div>';                    
+                    addHTML += '<div class = "col-sm-4 name-tag productCount">';
+                        addHTML += '<p>' + menus[i].cnt + '</p>';
+                    addHTML += '</div>';
+                    addHTML += '<div class = "col-sm-3 name-tag productPrice">';
+                        addHTML += '<p>' + menus[i].price + '</p>';
+                    addHTML += '</div>';
                 addHTML += '</div>';
-                addHTML += '<div class = "col-sm-3 name-tag productPrice">';
-                    addHTML += '<p>' + menus[i].price + '</p>';
-                addHTML += '</div>';
-            addHTML += '</div>';
-            $('div.row.product').after(addHTML);
-            break;
-        }else{
-            tempCnt = $('.productCount').val();
-            tempPrice = $('.productPrice').val();
+                $('div.row.product').after(addHTML);
+                break;
+            }
         }
-   }
+    }else{
+        alert('중복값 존재');
+    }
 });
 
+/* 
+else{
+           var tempCnt = $('.productCount').text();
+           var tempBefPrice = '';
+           var tempAftPrice = parseInt($('.productPrice').text());
+            $('.productCount').text(tempCnt++);
+            tempBefPrice = parseInt($('.productPrice').text());
+            $('.productPrice').text(tempBefPrice += tempAftPrice);
+        }
 
+*/
 
 //커피 클릭 이벤트
 $('#1').click(function (e) {
