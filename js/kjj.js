8dd49fbf9489a75b847e42236eeccd16b933291c
alikwon <63032589+alikwon@users.noreply.github.com>
2020-06-25 22:18:37 +0900

    수정ㅋㅋ

--- js/kjj.js
+++ js/kjj.js
@@ -1,199 +1,195 @@
 
-// // 동적 메뉴 생성
+// 동적 메뉴 생성
 
-// var coffees = [];
-// var drink = [];
-// var cof = [];
-// var cof = [];
-
-// function coffeeMenu(data) {
-//     var num = 10;
-//     var delNum = 0;
-    
-//     console.log(data)
-//     var menuPage = $('.1>.swiper-wrapper');
-//     for (var i=0; i < data.length; i++) {
-//         if (i % 4 == 0) {
-//             var html = '';
-//             html += '<div class="swiper-slide">';
-//             html += '<div class="container">';
-//             html += `<div class="row ml-4 mt-4 menu menuList" id="coffee${num}">`;
-//             html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
-//             html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             menuPage.append(html);
-//             num++;
-//         } else if (i%4 != 0) {
-//             var manu = $(`#coffee${num-1}`);
-//             var html2 = '';
-//             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
-//             html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html2 += '</div>';
-//             manu.append(html2);
-//         }
-        
-        
-//     }
-    
-// }
-// function drinkMenu(data) {
-//     var num = 10;
-//     var delNum = 0;
-    
-    
-//     var menuPage = $('.2>.swiper-wrapper');
-//     for (var i=0; i < data.length; i++) {
-//         if (i % 4 == 0) {
-//             var html = '';
-//             html += '<div class="swiper-slide">';
-//             html += '<div class="container">';
-//             html += `<div class="row ml-4 mt-4 menu menuList" id="drink${num}">`;
-//             html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
-//             html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             menuPage.append(html);
-//             num++;
-//         } else if (i%4 != 0) {
-//             var manu = $(`#drink${num-1}`);
-//             var html2 = '';
-//             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
-//             html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html2 += '</div>';
-//             manu.append(html2);
-//         }
-        
-        
-//     }
-    
-// }
-// function dessertMenu(data) {
-//     var num = 10;
-//     var delNum = 0;
-    
-    
-//     var menuPage = $('.3>.swiper-wrapper');
-//     for (var i=0; i < data.length; i++) {
-//         if (i % 4 == 0) {
-//             var html = '';
-//             html += '<div class="swiper-slide">';
-//             html += '<div class="container">';
-//             html += `<div class="row ml-4 mt-4 menu menuList" id="dessert${num}">`;
-//             html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
-//             html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             menuPage.append(html);
-//             num++;
-//             delNum++;
-//         } else if (i%4 != 0) {
-//             var manu = $(`#dessert${num-1}`);
-//             var html2 = '';
-//             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
-//             html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html2 += '</div>';
-//             manu.append(html2);
-//         }
-        
-        
-//     }
-    
-// }
-// function otherMenu(data) {
-//     var num = 10;
-//     var delNum = 0;
-    
-    
-//     var menuPage = $('.4>.swiper-wrapper');
-//     for (var i=0; i < data.length; i++) {
-//         if (i % 4 == 0) {
-//             var html = '';
-//             html += '<div class="swiper-slide">';
-//             html += '<div class="container">';
-//             html += `<div class="row ml-4 mt-4 menu menuList" id="othermenu${num}">`;
-//             html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
-//             html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             html += '</div>';
-//             menuPage.append(html);
-//             num++;
-//             delNum++;
-//         } else if (i%4 != 0) {
-//             var manu = $(`#othermenu${num-1}`);
-//             var html2 = '';
-//             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-//             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
-//             html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
-//             html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
-//             html2 += '</div>';
-//             manu.append(html2);
-//         }
-        
-        
-//     }
-    
-// }
+var coffees = [];
+var drinks = [];
+var dessert = [];
+var other = [];
 
 
-// // JSON 파일 로드 --> JS배열로 변환
-// function jsonParse() {
-//     $.getJSON('../json/coffee.json', function (data) {
-//         localStorage.setItem("coffees", JSON.stringify(data));
-//         coffees = JSON.parse(localStorage.getItem("coffees"));
-//         coffeeMenu(coffees);
+function coffeeMenu(data) {
+    var num = 10;
+    var delNum = 0;
+    
+    console.log(data)
+    var menuPage = $('.1>.swiper-wrapper');
+    for (var i=0; i < data.length; i++) {
+        if (i % 4 == 0) {
+            var html = '';
+            html += '<div class="swiper-slide">';
+            html += '<div class="container">';
+            html += `<div class="row ml-4 mt-4 menu menuList" id="coffee${num}">`;
+            html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
+            html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            menuPage.append(html);
+            num++;
+        } else if (i%4 != 0) {
+            var manu = $(`#coffee${num-1}`);
+            var html2 = '';
+            html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
+            html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html2 += '</div>';
+            manu.append(html2);
+        }
+        
+        
+    }
+    
+}
+function drinkMenu(data) {
+    var num = 10;
+    var delNum = 0;
+    
+    
+    var menuPage = $('.2>.swiper-wrapper');
+    for (var i=0; i < data.length; i++) {
+        if (i % 4 == 0) {
+            var html = '';
+            html += '<div class="swiper-slide">';
+            html += '<div class="container">';
+            html += `<div class="row ml-4 mt-4 menu menuList" id="drink${num}">`;
+            html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
+            html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            menuPage.append(html);
+            num++;
+        } else if (i%4 != 0) {
+            var manu = $(`#drink${num-1}`);
+            var html2 = '';
+            html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
+            html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html2 += '</div>';
+            manu.append(html2);
+        }
+        
+        
+    }
+    
+}
+function dessertMenu(data) {
+    var num = 10;
+    var delNum = 0;
+    
+    
+    var menuPage = $('.3>.swiper-wrapper');
+    for (var i=0; i < data.length; i++) {
+        if (i % 4 == 0) {
+            var html = '';
+            html += '<div class="swiper-slide">';
+            html += '<div class="container">';
+            html += `<div class="row ml-4 mt-4 menu menuList" id="dessert${num}">`;
+            html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
+            html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            menuPage.append(html);
+            num++;
+            delNum++;
+        } else if (i%4 != 0) {
+            var manu = $(`#dessert${num-1}`);
+            var html2 = '';
+            html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
+            html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html2 += '</div>';
+            manu.append(html2);
+        }
+        
+        
+    }
+    
+}
+function otherMenu(data) {
+    var num = 10;
+    var delNum = 0;
+    
+    
+    var menuPage = $('.4>.swiper-wrapper');
+    for (var i=0; i < data.length; i++) {
+        if (i % 4 == 0) {
+            var html = '';
+            html += '<div class="swiper-slide">';
+            html += '<div class="container">';
+            html += `<div class="row ml-4 mt-4 menu menuList" id="othermenu${num}">`;
+            html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html += `<img class="hvr-grow" src=${data[i].img} id=${data[i].id} class="rounded" onclick="choiceMenu(id)">`;
+            html += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            menuPage.append(html);
+            num++;
+            delNum++;
+        } else if (i%4 != 0) {
+            var manu = $(`#othermenu${num-1}`);
+            var html2 = '';
+            html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+            html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
+            html2 += `<div class="text-center mt-3 menuname">${data[i].name}</div>`;
+            html2 += `<div class="text-center menuprice"> &#8361;${data[i].price}</div>`;
+            html2 += '</div>';
+            manu.append(html2);
+        }
+        
+        
+    }
+    
+}
+// JSON 파일 로드 --> JS배열로 변환
+function jsonParse() {
+    $.getJSON('../json/coffee.json', function (data) {
+        localStorage.setItem("coffees", JSON.stringify(data));
+        coffees = JSON.parse(localStorage.getItem("coffees"));
+        coffeeMenu(coffees);
 
-//     });
-//     $.getJSON('../json/drink.json', function (data) {
-//         localStorage.setItem("drinks",JSON.stringify(data));
-//         drink = JSON.parse(localStorage.getItem("drinks"));
-//         drinkMenu(drink);
+    });
+    $.getJSON('../json/drink.json', function (data) {
+        localStorage.setItem("drinks",JSON.stringify(data));
+        drink = JSON.parse(localStorage.getItem("drinks"));
+        drinkMenu(drink);
         
-//     });
-//     $.getJSON('../json/dessert.json', function (data) {
-//         localStorage.setItem("dessert",JSON.stringify(data));
-//         dessert = JSON.parse(localStorage.getItem("dessert"));
-//         drinkMenu(dessert);
+    });
+    $.getJSON('../json/dessert.json', function (data) {
+        localStorage.setItem("dessert",JSON.stringify(data));
+        dessert = JSON.parse(localStorage.getItem("dessert"));
+        drinkMenu(dessert);
         
-//     });
-//     $.getJSON('../json/othermenu.json', function (data) {
-//         localStorage.setItem("other",JSON.stringify(data));
-//         other = JSON.parse(localStorage.getItem("other"));
-//         drinkMenu(other);
+    });
+    $.getJSON('../json/othermenu.json', function (data) {
+        localStorage.setItem("other",JSON.stringify(data));
+        other = JSON.parse(localStorage.getItem("other"));
+        drinkMenu(other);
    
-//     });
-// };
+    });
+};
 
-// function choiceMenu(menuid){
+
+
+function init() {
+    jsonParse();
     
-//     return id;
-// }
+}
 
-// function init() {
-//     jsonParse();
-    
-// }
-
-// init();
+init();
--- js/seongmin.js
+++ js/seongmin.js
@@ -1,64 +1,64 @@
 
-var menuPage = $('.1>.swiper-wrapper');
+// var menuPage = $('.1>.swiper-wrapper');
 
-// 동적 메뉴 생성
+// // 동적 메뉴 생성
 
-function initMenu(data) {
-    var num = 10;
-    var delNum = 0;
+// function initMenu(data) {
+//     var num = 10;
+//     var delNum = 0;
     
     
-    for (var i=0; i < data.length; i++) {
+//     for (var i=0; i < data.length; i++) {
         
-        if (i % 4 == 0) {
-            var html = '';
-            html += '<div class="swiper-slide">';
-            html += '<div class="container">';
-            html += `<div class="row ml-4 mt-4 menu menuList" id="${num}">`;
-            html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-            html += `<img class="hvr-grow" src=${data[i].img} class="rounded">`;
-            html += `<div class="text-center mt-3">${data[i].name}</div>`;
-            html += `<div class="text-center">&#8361;${data[i].price}</div>`;
-            html += '</div>';
-            html += '</div>';
-            html += '</div>';
-            html += '</div>';
-            menuPage.append(html);
-            num++;
-            delNum++;
-        } else if (i%4 != 0) {
-            console.log(delNum);
-            var manu = $(`#${num-1}`);
-            var html2 = '';
-            html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
-            html2 += `<img class="hvr-grow" src=${data[i].img} class="rounded">`;
-            html2 += `<div class="text-center mt-3">${data[i].name}</div>`;
-            html2 += `<div class="text-center">&#8361;${data[i].price}</div>`;
-            html2 += '</div>';
-            manu.append(html2);
-        }
+//         if (i % 4 == 0) {
+//             var html = '';
+//             html += '<div class="swiper-slide">';
+//             html += '<div class="container">';
+//             html += `<div class="row ml-4 mt-4 menu menuList" id="${num}">`;
+//             html += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+//             html += `<img class="hvr-grow" src=${data[i].img} class="rounded">`;
+//             html += `<div class="text-center mt-3">${data[i].name}</div>`;
+//             html += `<div class="text-center">&#8361;${data[i].price}</div>`;
+//             html += '</div>';
+//             html += '</div>';
+//             html += '</div>';
+//             html += '</div>';
+//             menuPage.append(html);
+//             num++;
+//             delNum++;
+//         } else if (i%4 != 0) {
+//             console.log(delNum);
+//             var manu = $(`#${num-1}`);
+//             var html2 = '';
+//             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
+//             html2 += `<img class="hvr-grow" src=${data[i].img} class="rounded">`;
+//             html2 += `<div class="text-center mt-3">${data[i].name}</div>`;
+//             html2 += `<div class="text-center">&#8361;${data[i].price}</div>`;
+//             html2 += '</div>';
+//             manu.append(html2);
+//         }
         
         
-    }
+//     }
     
-    console.log(manu.html());
-}
+//     console.log(manu.html());
+// }
 
 
-// JSON 파일 로드
-function jsonParse() {
-    $.getJSON('../json/coffee.json', function (data) {
-        var coffees = data;
-        initMenu(coffees);
+// // JSON 파일 로드
+// function jsonParse() {
+//     $.getJSON('../json/coffee.json', function (data) {
+//         var coffees = data;
+//         initMenu(coffees);
 
-    });
-};
+//     });
+// };
 
 
 
-function init() {
-    jsonParse();
-    console.log(coffee.length);
-}
+// function init() {
+//     jsonParse();
+//     console.log(coffee.length);
+// }
 
-init();
+// init();
