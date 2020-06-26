17e997d670338300b8873f8c2e11491460efa8b5
alikwon <63032589+alikwon@users.noreply.github.com>
2020-06-26 10:10:46 +0900

    end!

--- js/jungmin.js
+++ js/jungmin.js
@@ -1,88 +1,88 @@
-$(document).ready(function(){
-    var orderIDX = 0;
-    $('.menu1').click(function(){
+// $(document).ready(function(){
+//     var orderIDX = 0;
+//     $('.menu1').click(function(){
         
-        var thisName = $('div',this).first().text();
-        var thisPrice = $('div',this).last().text();
-        var thisNum = parseInt(1);
-        var addHTML = '';
+//         var thisName = $('div',this).first().text();
+//         var thisPrice = $('div',this).last().text();
+//         var thisNum = parseInt(1);
+//         var addHTML = '';
 
-        if($('div.order-list > .ordername').text() == thisName){
-            var befVal = $('div.order-list > .orderqty').text();
-            $('div.order-list > .orderqty').text(++befVal);
-        }else{
-            addHTML += '<div class="row order-list" id='+orderIDX+'>';
-                addHTML += '<div class="col-sm-1 delorder z">❌</div>';
-                addHTML += '<div class="col-sm-3 ordername z">' + thisName + '</div>';
-                addHTML += '<div class="col-sm-1 plusorder z">➕</div>';
-                addHTML += '<div class="col-sm-2 orderqty z">'+thisNum+'</div>';
-                addHTML += '<div class="col-sm-1 minusorder z">➖</div>';
-                addHTML += '<div class="col-sm-3 orderprice z">'+ thisPrice +'</div>';
-                addHTML += '<div class="col-sm-1 orderprice z">원</div>';
-            addHTML += '</div>';
-            $('.orderslide > div.container.productList').after(addHTML);
-            addHTML = '';
-            orderIDX++;
-        }
-    });
+//         if($('div.order-list > .ordername').text() == thisName){
+//             var befVal = $('div.order-list > .orderqty').text();
+//             $('div.order-list > .orderqty').text(++befVal);
+//         }else{
+//             addHTML += '<div class="row order-list" id='+orderIDX+'>';
+//                 addHTML += '<div class="col-sm-1 delorder z">❌</div>';
+//                 addHTML += '<div class="col-sm-3 ordername z">' + thisName + '</div>';
+//                 addHTML += '<div class="col-sm-1 plusorder z">➕</div>';
+//                 addHTML += '<div class="col-sm-2 orderqty z">'+thisNum+'</div>';
+//                 addHTML += '<div class="col-sm-1 minusorder z">➖</div>';
+//                 addHTML += '<div class="col-sm-3 orderprice z">'+ thisPrice +'</div>';
+//                 addHTML += '<div class="col-sm-1 orderprice z">원</div>';
+//             addHTML += '</div>';
+//             $('.orderslide > div.container.productList').after(addHTML);
+//             addHTML = '';
+//             orderIDX++;
+//         }
+//     });
 
 
-    $('.delorder').click(function(e){
-        //$('.order-list').empty();
-    });
+//     $('.delorder').click(function(e){
+//         //$('.order-list').empty();
+//     });
     
-    $('#id* .plusorder').click(function(e){
-        var befVal = $('.orderqty',this).text();
-        if (befVal>9) {
-            alert('주문을 더 추가하실 수 없습니다.');
-        }else{
-            $('.orderqty',this).text(++befVal);    
-        }
-    });
+//     $('#id* .plusorder').click(function(e){
+//         var befVal = $('.orderqty',this).text();
+//         if (befVal>9) {
+//             alert('주문을 더 추가하실 수 없습니다.');
+//         }else{
+//             $('.orderqty',this).text(++befVal);    
+//         }
+//     });
     
-    $('.minusorder').click(function(e){
-        var befVal = $('.orderqty').text();
-        if (befVal==1) {
-            $('.order-list').empty();
-        }else{
-            $('.orderqty').text(--befVal);    
-        }
-    });
+//     $('.minusorder').click(function(e){
+//         var befVal = $('.orderqty').text();
+//         if (befVal==1) {
+//             $('.order-list').empty();
+//         }else{
+//             $('.orderqty').text(--befVal);    
+//         }
+//     });
     
 
-});
-/*
+// });
+// /*
 
-function (e) {
-    var target = $('p',this).text();
-    var addHTML = '';
+// function (e) {
+//     var target = $('p',this).text();
+//     var addHTML = '';
  
-    if(document.getElementsByClassName('.productName').innerText != target){
-         for (var i = 0; i < menus.length; i++) {
-             if (target == menus[i].name && document.getElementById(menus[i].id) == null) {
-                 addHTML += '<div class = "row" id="' + menus[i].id + '">';
-                     addHTML += '<div class = "col-sm-5 name-tag productName">';
-                         addHTML += '<p>' + menus[i].name + '</p>';
-                     addHTML += '</div>';                    
-                     addHTML += '<div class = "col-sm-4 name-tag productCount">';
-                         addHTML += '<p>' + menus[i].cnt + '</p>';
-                     addHTML += '</div>';
-                     addHTML += '<div class = "col-sm-3 name-tag productPrice">';
-                         addHTML += '<p>' + menus[i].price + '</p>';
-                     addHTML += '</div>';
-                 addHTML += '</div>';
-                 $('div.row.product').after(addHTML);
-                 break;
-             }
-         }
-     }else{
-         alert('중복값 존재');
-     }
- }
-*/
-$('#6').click(function(e){
-    var delYN = confirm('주문내역을 전부 삭제하시겠습니까?');
-    if (delYN == true) {
-        $('.order-list').empty();
-    }
-});
+//     if(document.getElementsByClassName('.productName').innerText != target){
+//          for (var i = 0; i < menus.length; i++) {
+//              if (target == menus[i].name && document.getElementById(menus[i].id) == null) {
+//                  addHTML += '<div class = "row" id="' + menus[i].id + '">';
+//                      addHTML += '<div class = "col-sm-5 name-tag productName">';
+//                          addHTML += '<p>' + menus[i].name + '</p>';
+//                      addHTML += '</div>';                    
+//                      addHTML += '<div class = "col-sm-4 name-tag productCount">';
+//                          addHTML += '<p>' + menus[i].cnt + '</p>';
+//                      addHTML += '</div>';
+//                      addHTML += '<div class = "col-sm-3 name-tag productPrice">';
+//                          addHTML += '<p>' + menus[i].price + '</p>';
+//                      addHTML += '</div>';
+//                  addHTML += '</div>';
+//                  $('div.row.product').after(addHTML);
+//                  break;
+//              }
+//          }
+//      }else{
+//          alert('중복값 존재');
+//      }
+//  }
+// */
+// $('#6').click(function(e){
+//     var delYN = confirm('주문내역을 전부 삭제하시겠습니까?');
+//     if (delYN == true) {
+//         $('.order-list').empty();
+//     }
+// });
--- js/kjj.js
+++ js/kjj.js
@@ -5,15 +5,100 @@ var coffees = [];
 var drinks = [];
 var dessert = [];
 var other = [];
+var category = [];
+var sum = 0;
 
+//초기화버튼
+$(document).on('click', '#6', function () {
+    location.replace('index.html');
+});
 
+//휴지통버튼이벤트
+$(document).on('click', '.delorder', function (e) {
+    $(this).parent('.order-list').addClass('del');
+    setTimeout(function () {
+        document.querySelector(".del").remove();
+        // $(this).parent('.del').remove();
+        let sum = totalPrice($('.order-list').find('.orderprice'));
+        $('.finalprice-price').text(sum);
+    }, 300);
+});
+$(document).on('click', '.plusorder', function () {
+    //수량 ++
+    let $orderqty = $(this).parent().find('.orderqty');
+    let $orderprice = $(this).parent().find('.orderprice');
+    let currentprice = (parseInt($orderprice.text()) / parseInt($orderqty.text())) + parseInt($orderprice.text());
+    let currentQty = parseInt($orderqty.text()) + 1;
+    $orderprice.text(currentprice);
+    $orderqty.text(currentQty);
+    let sum = totalPrice($('.order-list').find('.orderprice'));
+    $('.finalprice-price').text(sum);
+});
+$(document).on('click', '.minusorder', function () {
+    let $order = $(this).parent('.order-list');
+    let $orderqty = $(this).parent().find('.orderqty');
+    let $orderprice = $(this).parent().find('.orderprice');
+    if (parseInt($orderqty.text()) > 1) {
+        let currentprice = parseInt($orderprice.text()) - (parseInt($orderprice.text()) / parseInt($orderqty.text()));
+        let currentQty = parseInt($orderqty.text()) - 1;
+        $orderprice.text(currentprice);
+        $orderqty.text(currentQty);
+    } else {
+        $order.remove();
+    }
+    let sum = totalPrice($('.order-list').find('.orderprice'));
+    $('.finalprice-price').text(sum);
+});
+
+function addOrder(match) {
+    var qty = 1;
+    // var a = ".order" + match.id;
+    var a = ".order" + match.id;
+    if ($(a).length === 0) {
+        let html = '';
+        html += '<div class="row order-list order' + match.id + '">'
+            html += '<div class="col-sm-1 delorder z">🗑️</div>';
+            html += '<div class="col-sm-3 ordername z">' + match.name + '</div>';
+            html += '<div class="col-sm-1 plusorder z">➕</div>';
+            html += '<div class="col-sm-2 orderqty z">' + qty + '</div>';
+            html += '<div class="col-sm-1 minusorder z">➖</div>';
+            html += '<div class="col-sm-3 orderprice z">' + match.price + '</div>';
+            html += '<div class="col-sm-1 won z">원</div>';
+        html += '</div>';
+        $('.productList').append(html);
+
+    } else {
+        var plusQty = qty += parseInt($(a + '>.orderqty').html());
+
+        var plusPrice = match.price + parseInt($(a + '>.orderprice').html());
+        $(a + '>.orderqty').text("" + plusQty);
+        $(a + '>.orderprice').text("" + plusPrice);
+    }
+    let sum = totalPrice($('.order-list').find('.orderprice'));
+    $('.finalprice-price').text(sum);
+
+}
+
+function choiceMenu(id) {
+    // console.log(id);
+    var match = [];
+    for (var i in category) {
+        match = category[i].filter(function (item) {
+            return item.id == id;
+        });
+        if (match.length != 0) {
+            break;
+        }
+    }
+    addOrder(match[0]);
+}
 function coffeeMenu(data) {
     var num = 10;
     var delNum = 0;
-    
+
     console.log(data)
     var menuPage = $('.1>.swiper-wrapper');
-    for (var i=0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         if (i % 4 == 0) {
             var html = '';
             html += '<div class="swiper-slide">';
@@ -29,8 +114,8 @@ function coffeeMenu(data) {
             html += '</div>';
             menuPage.append(html);
             num++;
-        } else if (i%4 != 0) {
-            var manu = $(`#coffee${num-1}`);
+        } else if (i % 4 != 0) {
+            var manu = $(`#coffee${num - 1}`);
             var html2 = '';
             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
@@ -39,18 +124,18 @@ function coffeeMenu(data) {
             html2 += '</div>';
             manu.append(html2);
         }
-        
-        
+
+
     }
-    
+
 }
 function drinkMenu(data) {
     var num = 10;
     var delNum = 0;
-    
-    
+
+
     var menuPage = $('.2>.swiper-wrapper');
-    for (var i=0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         if (i % 4 == 0) {
             var html = '';
             html += '<div class="swiper-slide">';
@@ -66,8 +151,8 @@ function drinkMenu(data) {
             html += '</div>';
             menuPage.append(html);
             num++;
-        } else if (i%4 != 0) {
-            var manu = $(`#drink${num-1}`);
+        } else if (i % 4 != 0) {
+            var manu = $(`#drink${num - 1}`);
             var html2 = '';
             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
@@ -76,18 +161,18 @@ function drinkMenu(data) {
             html2 += '</div>';
             manu.append(html2);
         }
-        
-        
+
+
     }
-    
+
 }
 function dessertMenu(data) {
     var num = 10;
     var delNum = 0;
-    
-    
+
+
     var menuPage = $('.3>.swiper-wrapper');
-    for (var i=0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         if (i % 4 == 0) {
             var html = '';
             html += '<div class="swiper-slide">';
@@ -104,8 +189,8 @@ function dessertMenu(data) {
             menuPage.append(html);
             num++;
             delNum++;
-        } else if (i%4 != 0) {
-            var manu = $(`#dessert${num-1}`);
+        } else if (i % 4 != 0) {
+            var manu = $(`#dessert${num - 1}`);
             var html2 = '';
             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
@@ -114,18 +199,18 @@ function dessertMenu(data) {
             html2 += '</div>';
             manu.append(html2);
         }
-        
-        
+
+
     }
-    
+
 }
 function otherMenu(data) {
     var num = 10;
     var delNum = 0;
-    
-    
+
+
     var menuPage = $('.4>.swiper-wrapper');
-    for (var i=0; i < data.length; i++) {
+    for (var i = 0; i < data.length; i++) {
         if (i % 4 == 0) {
             var html = '';
             html += '<div class="swiper-slide">';
@@ -142,8 +227,8 @@ function otherMenu(data) {
             menuPage.append(html);
             num++;
             delNum++;
-        } else if (i%4 != 0) {
-            var manu = $(`#othermenu${num-1}`);
+        } else if (i % 4 != 0) {
+            var manu = $(`#othermenu${num - 1}`);
             var html2 = '';
             html2 += `<div class="col-sm-5 ml-3 mt-3 menu1 ${data[i].id}">`;
             html2 += `<img class="hvr-grow" src=${data[i].img} id ="${data[i].id}" class="rounded" onclick="choiceMenu(id)">`;
@@ -152,10 +237,17 @@ function otherMenu(data) {
             html2 += '</div>';
             manu.append(html2);
         }
-        
-        
+
+
     }
-    
+
+}
+function totalPrice($tag) {
+    sum = 0;
+    $tag.each(function (index, item) {
+        sum += parseInt($(item).text());
+    });
+    return sum;
 }
 // JSON 파일 로드 --> JS배열로 변환
 function jsonParse() {
@@ -163,25 +255,29 @@ function jsonParse() {
         localStorage.setItem("coffees", JSON.stringify(data));
         coffees = JSON.parse(localStorage.getItem("coffees"));
         coffeeMenu(coffees);
+        category.push(coffees);
 
     });
     $.getJSON('../json/drink.json', function (data) {
-        localStorage.setItem("drinks",JSON.stringify(data));
+        localStorage.setItem("drinks", JSON.stringify(data));
         drink = JSON.parse(localStorage.getItem("drinks"));
         drinkMenu(drink);
-        
+        category.push(drink);
+
     });
     $.getJSON('../json/dessert.json', function (data) {
-        localStorage.setItem("dessert",JSON.stringify(data));
+        localStorage.setItem("dessert", JSON.stringify(data));
         dessert = JSON.parse(localStorage.getItem("dessert"));
         dessertMenu(dessert);
-        
+        category.push(dessert);
+
     });
     $.getJSON('../json/othermenu.json', function (data) {
-        localStorage.setItem("other",JSON.stringify(data));
+        localStorage.setItem("other", JSON.stringify(data));
         other = JSON.parse(localStorage.getItem("other"));
         otherMenu(other);
-   
+        category.push(other);
+
     });
 };
 
@@ -189,7 +285,6 @@ function jsonParse() {
 
 function init() {
     jsonParse();
-    
 }
 
 init();
